04c22bfc3483791d2d8a4bfc12bcb9a05a2d9dae
U-DESKTOP-15H9278\gharriso <guy.a.harrison@gmail.com>
2021-10-07 11:28:08 +1100

    Windows binaries

--- makeWin.bat
+++ makeWin.bat
@@ -1,11 +1,13 @@
 @echo on
-cp node_modules\oracledb\build\Release\oracledb-5.1.0-win32-x64.node dist
+cp node_modules/open/xdg-open dist 
+cp node_modules/keytar/build/Release/keytar.node dist
 
  pkg --options max_old_space_size=8192 --out-path dist -t node12-win . 
 
 cd dist
 
- "c:\program files\7-zip\7z" a -y  provendb-oracle-windows.zip provendb-oracle.exe oracledb-5.1.0-win32-x64.node
+ "c:\program files\7-zip\7z" a -y  provendb-sqlserver-windows.zip provendb-sqlserver.exe  keytar.node xdg-open
 
-del provendb-oracle.exe
-del oracledb-5.1.0-win32-x64.node
+ del provendb-sqlserver.exe
+
+ 
